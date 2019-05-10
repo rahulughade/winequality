478aaa63019a0d62c7c4a950a3a3fff8bd95ba0c
Rahul Ughade <rahulughade@gmail.com>
2019-05-10 08:58:00 -0500

    Adding flask files

---
+++ script.py
@@ -0,0 +1,66 @@
+#importing libraries
+import os
+import numpy as np
+import flask
+import pickle
+from flask import Flask, render_template, request
+
+#creating instance of the class
+app=Flask(__name__)
+
+#to tell flask what url shoud trigger the function index()
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+#prediction function for red wine
+def Red_ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,6)
+    loaded_model = pickle.load(open("tuned_redmodel.pkl","rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+    
+
+#prediction function for white wine
+def White_ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,5)
+    loaded_model = pickle.load(open("tuned_whitemodel.pkl","rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+    
+
+@app.route('/result_red',methods = ['POST'])
+def result_red():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        result = Red_ValuePredictor(to_predict_list)
+        print(to_predict_list)
+        
+        if int(result)==0:
+            prediction='Poor Wine'
+        elif int(result)==1:
+            prediction='Average Wine'
+        else:
+            prediction='Great Wine'
+            
+        return render_template("result_red.html",prediction_red=prediction)
+
+@app.route('/result_white',methods = ['POST'])
+def result_white():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        result = White_ValuePredictor(to_predict_list)
+        
+        if int(result)==0:
+            prediction='Poor Wine'
+        elif int(result)==1:
+            prediction='Average Wine'
+        else:
+            prediction='Great Wine'
+            
+        return render_template("result_white.html",prediction_white=prediction)
---
+++ static/js/guage_white.js
@@ -0,0 +1,68 @@
+// Enter a speed between 0 and 180
+// var level = 125;
+if (pred_white === 'Poor Wine') {
+  level = 45;
+}
+else if (pred_white === 'Average Wine'){
+    level = 90;
+  }
+  
+else level = 135;
+
+
+// Trig to calc meter point
+var degrees = 180 - level,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var data = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 28, color:'850000'},
+    showlegend: false,
+    name: 'rotation',
+    text: level,
+    hoverinfo: 'text'},
+  {values: [50/3, 50/3, 50/3, 50],
+  rotation: 90,
+  text: ['GREAT', 'AVERAGE', 'BAD', ''],
+  textinfo: 'text',
+  textposition:'inside',      
+  marker: {colors:['RGBA(0, 128, 0, .9)', 'rgba(255, 195, 0, .8)',
+                         'rgba(199, 0, 57, .9)', 
+                         'rgba(255, 255, 255, 0)']},
+  labels: ['2', '1', '0', ''],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: false
+}];
+
+var layout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: '850000',
+      line: {
+        color: '850000'
+      }
+    }],
+  title: '<br><b>Wine Gauge</b>',
+  height: 1000,
+  width: 1000,
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('WhiteDiv', data, layout, {showSendToCloud:true});
